efa64438c73e5a946c23b4bdc3887916de73b601
segerard <sarah-gerard@uiowa.edu>
2015-10-06 16:05:34 +0000

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  def users_params
+  def user_params
     params.require(:user).permit(:user_id, :email, :session_token, :timestamps)
   end
   
@@ -9,15 +9,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    check_user_id = User.find params[:user_id] 
-    if(check_user_id == nil)
-      flash[:notice] = "Sorry, this user-id is taken. Try again."
-      redirect_to new_user_path
-    else
-      User.create_user!()
       @user = User.create!(user_params)
-      flash[:notice] = "Welcome #{@user.user_id}. Your account has been created."
-      redirect_to movies_path
-    end
+#      if(@user.session_token != nill)
+        flash[:notice] = "Welcome #{@user.user_id}. Your account has been created."
+        redirect_to movies_path
+#      end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+  def create_user!(params) 
+#   u = User.all
+#   check_user_id = u.index(params[:user_id])
+#   if(check_user_id == nil)
+#       flash[:notice] = "Sorry, this user-id is taken. Try again."
+#       redirect_to new_user_path
+#   else    
+     params[:session_token]="123"
+     User.create!(params)
+#   end
+ end
+
 end
