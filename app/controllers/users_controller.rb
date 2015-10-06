560bfccbacfaba6616fe7f6e9a6f9ffd8cdca683
Sarah Gerard <sarah-gerard@uiowa.edu>
2015-10-06 14:10:52 +0200

    added create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
 
- # def users_params
- #   params.require(:movie).permit(:title, :rating, :description, :release_date)
- # end
+  def users_params
+    params.require(:user).permit(:user_id, :email, :session_token, :timestamps)
+  end
 
   def new
     # default: render 'new' template
   end
 
-#  def create
-#    @user = User.create!(movie_params)
-#    flash[:notice] = "#{@movie.title} was successfully created."
-#    redirect_to movies_path
-#  end
+  def create
+    @user = User.create!(user_params)
+    flash[:notice] = "Welcome #{@user.user_id}. Your account has been created."
+    redirect_to movies_path
+  end
 end
