fc045c1253966333ffc6b389328d6a0af502643f
Sarah Gerard <sarah-gerard@uiowa.edu>
2015-10-11 10:19:56 -0500

    edit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def session_params
-    params.require(:user).permit(:user_id, :email)
+    params.require(:session).permit(:user_id, :email)
   end
   
   def new
